5fdd9deb8538d5867c6d0c88a4087af0d9ad95e3
DanielRond <danieldossantos1818@gmail.com>
2026-08-22 13:22:55 -0300

    feat: construindo estrutura laravel

---
+++ backend/app/Models/Cliente.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Cliente extends Model
+{
+    protected $fillable = ['nome', 'email'];
+    public function pedidos(): HasMany
+    {
+        return $this->hasMany(Pedido::class);
+    }
+}
